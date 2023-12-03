3d9e8fc6fbe3b840e7719853a3355fb02ddcb1a2
RAIAN EL-CHABE <raianesmecher@gmail.com>
2023-12-03 14:30:24 +0200

    I don't know, now it works

--- phplol2.php
+++ phplol2.php
@@ -200,7 +200,7 @@ function fibonacci($n) {
 }
 
 function factorial($n) {
-    if ($n === 0 || $n === 1) {
+    if ($n == 0 || $n == 1) {
         return 1;
     } else {
         return $n * factorial($n - 1);
